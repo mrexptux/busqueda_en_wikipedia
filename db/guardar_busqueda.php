ceca33dd3991cc56a13c0a28dae6bc48625ef726
Pol Aguilar <polete1234@hotmail.com>
2024-06-12 16:52:36 +0200

    Añadimos retoques.

--- db/guardar_busqueda.php
+++ db/guardar_busqueda.php
@@ -2,24 +2,24 @@
 
 include("../src/config.php");
 
-//
+//POL: Leemos el cuerpo recibido por POST y lo transformamos a array, posteriormente elegimos el parametro recibido por POST.
 $data = json_decode(file_get_contents('php://input'), true);
 $termino = $data['termino'];
 
 
-//Preparamos la query y utilizamos tecnicas contra inyecciones.
+//POL: Preparamos la query y utilizamos tecnicas contra inyecciones.
 $stmt = $conn->prepare("INSERT INTO busquedas (termino) VALUES (?)");
 $stmt->bind_param("s", $termino);
 
 
-//Con el execute ejecutamos la query anterior y verficamos si se realiza bien la inseccion.
+//POL: Con el execute ejecutamos la query anterior y verficamos si se realiza bien la inseccion.
 if ($stmt->execute()) {
     echo json_encode(['success' => 'El término de búsqueda se guardó correctamente.']);
 } else {
     echo json_encode(['error' => 'Error al guardar el término de búsqueda: ' . $stmt->error]);
 }
 
-//cerramos la conexion.
+//POL: Cerramos la conexion y la query.
 $stmt->close();
 $conn->close();
 
--- src/config.php
+++ src/config.php
@@ -7,16 +7,16 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
 
-//Configuracion parametros del servidor. 
+//POL: Configuracion parametros del servidor. 
 $servername = "localhost"; // Url donde tenemos que apuntar para poder realizar la conexion.
 $username = "root"; // User del servidor.
 $password = ""; //PW del servidor. 
 $dbname = "historial_busqueda"; //Base de datos que hacemos referencia para realizar las acciones pedidas.
 
-//Llamamos a la conexion.
+//POL: Llamamos a la conexion.
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-//Verificamos si la conexion a sido existosa con el parametro que nos ofrece mysqli->connect_error.
+//POL: Verificamos si la conexion a sido existosa con el parametro que nos ofrece mysqli->connect_error.
 if ($conn->connect_error) {
     die(json_encode(['error' => 'Connection failed: ' . $conn->connect_error]));
 }
