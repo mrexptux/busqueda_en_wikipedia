453d0e52f68fca5cebd92e6511fe6b78b27f2d28
Pol Aguilar <polete1234@hotmail.com>
2024-06-13 16:51:24 +0200

    Añadido configuracion global.

--- env.php
+++ env.php
@@ -1,7 +1,47 @@
 <?php
-// Definir las constantes para las credenciales de la base de datos
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'historial_busqueda');
+
+/**
+ * Carga las variables de entorno desde el archivo .env
+ *
+ * @param string $path Ruta al archivo .env
+ * @return array Las variables de entorno cargadas
+ */
+function load_env($path)
+{
+    $variables = [];
+
+    // Verificar si el archivo .env existe
+    if (!file_exists($path)) {
+        throw new Exception("El archivo .env no existe.");
+        alert("El archivo .env no existe..");
+    }
+
+    // Leer líneas del archivo .env
+    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    foreach ($lines as $line) {
+        // Ignorar líneas que comiencen con # (comentarios)
+        if (strpos(trim($line), '#') === 0) {
+            continue;
+        }
+
+        // Dividir la línea en nombre y valor de variable
+        list($key, $value) = explode('=', $line, 2);
+
+        // Limpiar y asignar la variable de entorno
+        $key = trim($key);
+        $value = trim($value);
+        putenv("$key=$value");
+        $variables[$key] = $value;
+    }
+
+    return $variables;
+}
+
+// Ruta al archivo .env (ajusta según la ubicación de tu archivo .env)
+$dotenv_path = __DIR__ . '/.env';
+
+// Cargar las variables de entorno desde el archivo .env
+$env_vars = load_env($dotenv_path);
+
 ?>
--- src/config.php
+++ src/config.php
@@ -3,6 +3,12 @@
 //Llamamos a las credenciales de .env y asi poder configurar correctamente los parametros de conexion.
 require_once('../env.php');
 
+//Parametros de configuracion de archivo .env
+$db_host = getenv('DB_HOST');
+$db_user = getenv('DB_USER');
+$db_password = getenv('DB_PASSWORD');
+$db_name = getenv('DB_NAME');
+
 //Esta parte es muy importante ya que estamos configurando el tema de el contenido de datos y el tema de las cors.
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
@@ -10,14 +16,8 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
 
-//POL: Configuracion parametros del servidor. 
-$servername = "localhost"; // Url donde tenemos que apuntar para poder realizar la conexion.
-$username = "root"; // User del servidor.
-$password = ""; //PW del servidor. 
-$dbname = "historial_busqueda"; //Base de datos que hacemos referencia para realizar las acciones pedidas.
-
 //POL: Llamamos a la conexion.
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 
 //POL: Verificamos si la conexion a sido existosa con el parametro que nos ofrece mysqli->connect_error.
 if ($conn->connect_error) {
